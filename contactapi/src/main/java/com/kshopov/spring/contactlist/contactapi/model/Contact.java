818eaccd2f723d40d7c08ffc94568cb51f63103d
kshopov <krasimir.shopov@gmail.com>
2023-12-13 16:01:35 +0200

    Contact model

---
+++ contactapi/src/main/java/com/kshopov/spring/contactlist/contactapi/model/Contact.java
@@ -0,0 +1,35 @@
+package com.kshopov.spring.contactlist.contactapi.model;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name = "contacts")
+public class Contact {
+	@Id
+	@UuidGenerator
+	@Column(name = "id", unique = true, updatable = false)
+    private String id;
+    private String name;
+    private String email;
+    private String title;
+    private String phone;
+    private String address;
+    private boolean isActive;
+    private String photoUrl;
+}
